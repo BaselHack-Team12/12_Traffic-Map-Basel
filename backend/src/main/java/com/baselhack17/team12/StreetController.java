3a1a04425c41369d0fcac01b0426d3e37601e9ca
ilgun <ilgun.ilgun@magnolia-cms.com>
2017-10-29 09:16:44 +0100

    Fix getStreets

--- backend/src/main/java/com/baselhack17/team12/StreetController.java
+++ backend/src/main/java/com/baselhack17/team12/StreetController.java
@@ -1,22 +1,16 @@
 package com.baselhack17.team12;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * //TODO write here something nicer.
  */
 @RestController
 public class StreetController {
-
-    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
-    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "hello";
-    }
 
     @RequestMapping(value = "/streets", method = RequestMethod.GET)
     public List<streets> getStreets() {
