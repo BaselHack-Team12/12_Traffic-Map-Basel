66a21b05e1f767d8c1eb9209973ddb1bef89a9ea
Vincent Perrin <vpcom01@gmail.com>
2017-10-29 13:16:23 +0100

    implemented a service with fake and real implementation, street object first draft, and it is using Promises.

--- frontend/trafficView/src/app/app.module.ts
+++ frontend/trafficView/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { MapBlockComponent } from './map-block/map-block.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule }    from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { StreetDetailsComponent } from './street-details/street-details.component';
 import { ListBlockComponent } from './list-block/list-block.component';
 import { MainPageComponent } from './main-page/main-page.component';
+
+import { MapDataService } from './map-data/map-data.service';
 
 @NgModule({
   declarations: [
@@ -21,9 +24,10 @@ import { MainPageComponent } from './main-page/main-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpModule,
     NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [MapDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/trafficView/src/app/map-block/map-block.component.ts
+++ frontend/trafficView/src/app/map-block/map-block.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MapDataService } from '../map-data/map-data.service';
+import { Street } from '../map-data/street';
+
+
 @Component({
   selector: 'app-map-block',
   templateUrl: './map-block.component.html',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapBlockComponent implements OnInit {
 
-  constructor() { }
+  streets: Street[] = [];
+  
+  constructor(private mapDataService: MapDataService) { }
+
+  getStreets(): void {
+    console.log("request ...");
+
+    this.mapDataService.getStreets().then(streets => this.streets = streets);
+  }
 
   ngOnInit() {
+    this.getStreets();
+
   }
 
 }
